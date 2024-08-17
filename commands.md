3ec7adb676f560de99c27b510f49b821bd13e4fd
danish0511 <dsdd0511@gmail.com>
2024-08-18 00:43:06 +0530

    setup

---
+++ commands.md
@@ -0,0 +1,22 @@
+
+```
+docker compose up
+```
+
+```
+celery -A webscrape worker -l INFO
+celery -A webscrape --beat
+```
+
+```
+python manage.py shell
+```
+
+```
+python manage.py runserver
+```
+
+```
+jupyter notebook
+```
+
--- src/webscrape/settings.py
+++ src/webscrape/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2w6&fxc!cft81+*+1e9f5goiytm^m_zr68a2p_opisbryn36(f"
+SECRET_KEY = config("DJANGO_SECRET_KEY", default=None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool, default=False)
 
 ALLOWED_HOSTS = []
 
